d0fc88662af3a7bbe254878850ff8ce165834ddc
Alex Naidis <alex.naidis@linux.com>
2017-06-30 16:53:01 +0000

    oneplus3: add QPerformance to Boot jars
    * improves latency of fw perflocks.
    
    Signed-off-by: joshuous <joshuous@gmail.com>

--- device.mk
+++ device.mk
@@ -300,6 +300,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libjson
 
+# QPerformance
+PRODUCT_PACKAGES += \
+	PRODUCT_BOOT_JARS += QPerformance
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     fstab.qcom \
