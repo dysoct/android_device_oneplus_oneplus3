16864e8f57e525ff4f644813208ef54d27f3a694
INT3NSE07 <d.jonathanpaul@gmail.com>
2017-09-03 20:41:49 +0530

    op3: add prebuilts from purefusion Change-Id: Id4684e63e8bf8095ecaf3a24d351fa178a603e23

--- device.mk
+++ device.mk
@@ -278,6 +278,25 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:system/etc/powerhint.xml
 
+
+# Prebuilts
+PRODUCT_PACKAGES += \
+    OnePlusMusic \
+    OnePlusCamera \
+    OnePlusCameraService \
+    OnePlusGallery \
+    GoogleCamera
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/lib/libcameraservice.so:system/lib/libcameraservice.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libcameraservice.so:system/lib64/libcameraservice.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libfilter-sdk.so:system/lib64/libfilter-sdk.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopbaselib.so:system/lib64/libopbaselib.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopcamera.so:system/lib64/libopcamera.so \
+    $(LOCAL_PATH)/prebuilt/lib64/libopcameralib.so:system/lib64/libopcameralib.so
+#   $(LOCAL_PATH)/prebuilt/lib/libmediaplayerservice.so:system/lib/libmediaplayerservice.so \
+#   $(LOCAL_PATH)/prebuilt/lib64/libmediaplayerservice.so:system/lib64/libmediaplayerservice.so \
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
